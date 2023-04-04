59afa185c0ab46984c0eceb80c71319e822a344a
Raisa320 <67403486+Raisa320@users.noreply.github.com>
2023-04-03 20:40:32 -0500

    ejercicio 1

--- seccion-1/variables/sistema-solar.go
+++ seccion-1/variables/sistema-solar.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var lunasMercurio, lunasVenus int = 0, 0
+	lunasTierra := 1
+	var lunasMarte int = 2
+	var lunasJupiter int = 63
+	var lunasSaturno int = 62
+	var lunasUrano int = 27
+	var lunasNeptuno int = 13
+
+	fmt.Printf("Mercurio tiene %v lunas. \n", lunasMercurio)
+	fmt.Printf("Venus tiene %v lunas.\n", lunasVenus)
+	fmt.Printf("Tierra tiene %v lunas.\n", lunasTierra)
+	fmt.Printf("Marte tiene %v lunas.\n", lunasMarte)
+	fmt.Printf("Júpiter tiene %v lunas.\n", lunasJupiter)
+	fmt.Printf("Saturno tiene %v lunas.\n", lunasSaturno)
+	fmt.Printf("Urano tiene %v lunas.\n", lunasUrano)
+	fmt.Printf("Neptuno tiene %v lunas.\n", lunasNeptuno)
 }
