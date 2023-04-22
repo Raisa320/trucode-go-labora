8aee24a470cdcfbbf42a89b527ea86ae618c4048
Raisa320 <67403486+Raisa320@users.noreply.github.com>
2023-04-22 12:35:00 -0500

    intento mensajeria 1

--- seccion-2/channels/pairProgramming/ejercicio3.go
+++ seccion-2/channels/pairProgramming/ejercicio3.go
@@ -1,52 +1,113 @@
 package main
+
 import (
-    "fmt"
-    "time"
+	"fmt"
+	"time"
 )
+
 type Message struct {
-    typ     string
-    content string
+	Type    string
+	Content string
 }
+
 func main() {
-    chanel := make(chan Message)
-    for {
-        var mensaje Message
-        fmt.Println("(Para salir escriba salir)")
-        fmt.Println("Ingrese el tipo de mensaje: ")
-        fmt.Scan(&mensaje.typ)
-        if mensaje.typ == "salir" {
-            break
-        }
-        fmt.Println("Ingrese el contenido del mensaje: ")
-        fmt.Scan(&mensaje.content)
-        if mensaje.typ == "email" {
-            go setEmail(mensaje, chanel)
-            time.Sleep(time.Second)
-            confirmMessage(chanel)
-        }
-        if mensaje.typ == "SMS" {
-            go setSms(mensaje, chanel)
-            time.Sleep(time.Second)
-            confirmMessage(chanel)
-        }
-        if mensaje.typ == "push" {
-            go setPush(mensaje, chanel)
-            time.Sleep(time.Second)
-            confirmMessage(chanel)
-        }
-    }
-    close(chanel)
+	channelEmail := make(chan Message)
+	channelPush := make(chan Message)
+	channelSms := make(chan Message)
+	stopEmail := false
+	stopPush := false
+	stopSms := false
+
+	for {
+		var message Message
+		var typeMessage string
+		time.Sleep(time.Second)
+
+		fmt.Println("Ingrese el tipo de mensaje (email, sms, push) o alguna de estas opciones (salir, pausar, reanudar) ")
+		fmt.Scanln(&typeMessage)
+
+		if typeMessage == "salir" {
+			break
+		}
+
+		if typeMessage == "pausar" || typeMessage == "reanudar" {
+			optionsSecondary(&stopEmail, &stopPush, &stopSms, typeMessage)
+			//fmt.Println(stopEmail)
+		} else {
+			message.Type = typeMessage
+			fmt.Println("Ingrese el contenido del mensaje: ")
+			fmt.Scanln(&message.Content)
+			sendMessage(message, channelEmail, channelPush, channelSms, &stopEmail, &stopSms, &stopPush)
+			//time.Sleep(time.Second)
+		}
+	}
+	close(channelEmail)
+	close(channelPush)
+	close(channelSms)
 }
-func setEmail(message Message, chanel chan Message) {
-    chanel <- message
+func menuBase() {
+	fmt.Println("1. Email")
+	fmt.Println("2. SMS")
+	fmt.Println("3. Push")
+	fmt.Print("Elija una opción: ")
 }
-func confirmMessage(c chan Message) {
-    mensaje := <-c
-    fmt.Println("Mensaje recibido", mensaje)
+
+func sendMessage(message Message, channelEmail, channelPush, channelSms chan Message, stopEmail, stopSms, stopPush *bool) {
+	if message.Type == "email" {
+		go setMessageToChannel(message, channelEmail)
+		time.Sleep(time.Second)
+		go getMessageFromChannel(channelEmail, stopEmail)
+
+	}
+	if message.Type == "sms" {
+		go setMessageToChannel(message, channelSms)
+		time.Sleep(time.Second)
+		go getMessageFromChannel(channelSms, stopSms)
+	}
+	if message.Type == "push" {
+		go setMessageToChannel(message, channelPush)
+		time.Sleep(time.Second)
+		go getMessageFromChannel(channelPush, stopPush)
+	}
 }
-func setSms(message Message, chanel chan Message) {
-    chanel <- message
+
+func optionsSecondary(stopEmail, stopPush, stopSms *bool, option string) {
+	menuBase()
+	var typeMessage string
+	fmt.Scanln(&typeMessage)
+	if option == "reanudar" {
+		if typeMessage == "1" {
+			*stopEmail = false
+		}
+		if typeMessage == "2" {
+			*stopSms = false
+		}
+		if typeMessage == "3" {
+			*stopPush = false
+		}
+	} else if option == "pausar" {
+		if typeMessage == "1" {
+			*stopEmail = true
+		}
+		if typeMessage == "2" {
+			*stopSms = true
+		}
+		if typeMessage == "3" {
+			*stopPush = true
+		}
+	}
+
 }
-func setPush(message Message, chanel chan Message) {
-    chanel <- message
+
+func setMessageToChannel(message Message, chanel chan Message) {
+	fmt.Println(message.Type, " recibido")
+	chanel <- message
+}
+func getMessageFromChannel(c chan Message, pausa *bool) {
+	for !*pausa {
+		select {
+		case mensaje := <-c:
+			fmt.Println("Mensaje procesado", mensaje, *pausa)
+		}
+	}
 }
