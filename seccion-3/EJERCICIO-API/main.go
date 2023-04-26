06026a121e3e57e36ce4324cd967b25886a60efb
Raisa320 <67403486+Raisa320@users.noreply.github.com>
2023-04-26 18:57:09 -0500

    agregando nuevos endpoints

--- seccion-3/EJERCICIO-API/main.go
+++ seccion-3/EJERCICIO-API/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/gorilla/mux"
 )
@@ -69,12 +70,13 @@ func main() {
 	router.HandleFunc("/items/{id}", getItem).Methods("GET")
 
 	//Más adelante.
+	router.HandleFunc("/item", searchItem).Methods("GET")
 	router.HandleFunc("/items", createItem).Methods("POST")
 	router.HandleFunc("/items/{id}", updateItem).Methods("PUT")
 	router.HandleFunc("/items/{id}", deleteItem).Methods("DELETE")
 
 	portNumber := 9000
-	fmt.Printf("Listen in port %d", portNumber)
+	fmt.Printf("Listen in port %d\n", portNumber)
 	err := http.ListenAndServe(":"+strconv.Itoa(portNumber), router)
 	if err != nil {
 		fmt.Println(err)
@@ -82,12 +84,31 @@ func main() {
 }
 
 func getItems(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("Content-Type", "application/json")
 	// Función para obtener todos los elementos
 	json.NewEncoder(response).Encode(items)
 }
 
+func searchItem(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("Content-Type", "application/json")
+	paramName := request.URL.Query()
+	name, err := paramName["name"]
+	if !err {
+		response.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	var itemsFound []Item
+	for _, item := range items {
+		if strings.Contains(item.Name, name[0]) {
+			itemsFound = append(itemsFound, item)
+		}
+	}
+	json.NewEncoder(response).Encode(itemsFound)
+}
+
 func getItem(response http.ResponseWriter, request *http.Request) {
 	// Función para obtener un elemento específico
+	response.Header().Set("Content-Type", "application/json")
 	param := mux.Vars(request)
 	idItem := param["id"]
 	for _, item := range items {
@@ -100,14 +121,57 @@ func getItem(response http.ResponseWriter, request *http.Request) {
 	json.NewEncoder(response).Encode(Response{"item not found"})
 }
 
-func createItem(w http.ResponseWriter, r *http.Request) {
+func createItem(response http.ResponseWriter, request *http.Request) {
 	// Función para crear un nuevo elemento
+	response.Header().Set("Content-Type", "application/json")
+	var item Item
+	err := json.NewDecoder(request.Body).Decode(&item)
+	if err != nil {
+		http.Error(response, err.Error(), http.StatusBadRequest)
+		return
+	}
+	items = append(items, item)
+	response.WriteHeader(http.StatusCreated)
+	json.NewEncoder(response).Encode(items)
 }
 
-func updateItem(w http.ResponseWriter, r *http.Request) {
+func updateItem(response http.ResponseWriter, request *http.Request) {
 	// Función para actualizar un elemento existente
+	response.Header().Set("Content-Type", "application/json")
+	var itemUpdate Item
+	err := json.NewDecoder(request.Body).Decode(&itemUpdate)
+	if err != nil {
+		http.Error(response, err.Error(), http.StatusBadRequest)
+		return
+	}
+	param := mux.Vars(request)
+	idItem := param["id"]
+	for indice, item := range items {
+		if item.ID == idItem {
+			items[indice] = itemUpdate
+			response.WriteHeader(http.StatusOK)
+			json.NewEncoder(response).Encode(items)
+			return
+		}
+	}
+	response.WriteHeader(http.StatusNotFound)
+	json.NewEncoder(response).Encode(Response{"item not found"})
 }
 
-func deleteItem(w http.ResponseWriter, r *http.Request) {
+func deleteItem(response http.ResponseWriter, request *http.Request) {
 	// Función para eliminar un elemento
+	response.Header().Set("Content-Type", "application/json")
+	param := mux.Vars(request)
+	idItem := param["id"]
+	for indice, item := range items {
+		if item.ID == idItem {
+			items = append(items[:indice], items[indice+1:]...)
+			response.WriteHeader(http.StatusOK)
+			json.NewEncoder(response).Encode(items)
+			return
+		}
+	}
+	response.WriteHeader(http.StatusNotFound)
+	json.NewEncoder(response).Encode(Response{"item not found"})
+
 }
